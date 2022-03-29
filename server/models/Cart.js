4c20216eb784a0450ff00c0e654186631ce0b9dc
Chinwendu <chinwenduukoha@gmail.com>
2022-03-29 15:05:45 -0400

    got first set of controllers to work

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -2,10 +2,8 @@ const { Schema } = require('mongoose')
 
 const Cart = new Schema(
   {
-    image: { type: String, required: true },
     title: { type: String, required: true },
     items: [{ type: Schema.Types.ObjectId, ref: 'Item', required: true }],
-    quantitySelected: { type: Number, min: 0, required: false },
     reservedStatus: { type: Boolean, required: true }
   },
   { timestamps: true }
