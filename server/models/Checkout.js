38a9dcb2571b292134ee8640a543b7368ff21ab6
Chinwendu <chinwenduukoha@gmail.com>
2022-03-30 07:40:53 -0400

    wednesday morning

--- server/models/Checkout.js
+++ server/models/Checkout.js
@@ -2,14 +2,13 @@ const { Schema } = require('mongoose')
 
 const Checkout = new Schema(
   {
-    items: [{ type: Schema.Types.ObjectId, ref: 'Registry', required: false }],
     totalcost: { type: Number, required: true },
     giftMessage: { type: String, required: false },
-    email: { type: String, required: false },
-    paymentDetails: { type: Number, required: false },
-    name: { type: String, required: false },
-    address: { type: String, required: false },
-    phoneNumber: { type: Number, required: false },
+    email: { type: String, required: true },
+    paymentDetails: { type: Number, required: true },
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
     category: [
       { type: Schema.Types.ObjectId, ref: 'Registry', required: true }
     ],
