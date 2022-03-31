56acee5cb65bbbc532bd5e8c92b67166afec997f
Chinwendu <chinwenduukoha@gmail.com>
2022-03-30 21:37:24 -0400

    homepage done

--- client/clunion/src/pages/Home.js
+++ client/clunion/src/pages/Home.js
@@ -3,14 +3,24 @@ import React from 'react'
 // import { useEffect, useState } from 'react'
 import '../style/App.css'
 import HomeImage from '../highlights/HomeImage.png'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Registry from '../components/Registry'
 // import DateCountdown from 'react-date-countdown-timer'
 
 const Home = () => {
-  // const BASE_URL = 'http://localhost:3001/api'
+  const [registry, setRegistry] = useState([])
 
-  // useEffect(() => {
+  const BASE_URL = 'http://localhost:3001/api'
 
-  // },[])
+  useEffect(() => {
+    const getRegistry = async () => {
+      const response = await axios.get(`${BASE_URL}/registry`)
+      console.log(response.data, 'ALL REGISTRIES')
+      setRegistry(response.data)
+    }
+    getRegistry()
+  }, [])
   return (
     <div className="home">
       <img className="homeImage" src={HomeImage} alt="mainCoupleImage" />
@@ -20,6 +30,9 @@ const Home = () => {
         wedding, coming December 16, 2022, in Lagos, Nigeria. We hope you are as
         excited as we are!
       </h3>
+      <div>
+        <Registry registry={registry} />
+      </div>
       <div>
         <h4>December 16, 2022</h4>
         <h4>Oniru, Lagos, Nigeria</h4>
