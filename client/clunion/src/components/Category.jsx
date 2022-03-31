bc2e01305ff0d43b0d0e516a6956fe391efaa84a
Chinwendu <chinwenduukoha@gmail.com>
2022-03-31 14:45:56 -0400

    fixing categories

--- client/clunion/src/components/Category.jsx
+++ client/clunion/src/components/Category.jsx
@@ -1,22 +1,33 @@
 import axios from 'axios'
 import React from 'react'
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import Registry from './Registry'
 
 
 const Category = (props) => {
-  const [category, setCategory] = useState([])
+  const [categoryItems, setCategoryItems] = useState([])
 
-  useEffect( async () => {
- await axios.get(`${props.BASE_URL}/category/${props.id}`)
-  }, [])
+  useEffect(() => {
+    let isCancelled = false
+    const getItemsByCategoryId = async () => {
+      const response = await axios.get(`${props.BASE_URL}/category/${props.id}/items`)
+      if (!isCancelled) {
+        setCategoryItems(response.data)
+      }
+    } 
+    getItemsByCategoryId()
+    return() => {
+      isCancelled = true
+    }
+  }, [props.id])
 
   return (
-    <div className='selectCategory'>
+    <div className='selectCategoryItems'>
       <button>
-        {category.map((cat) => (
+        {categoryItems.map((items) => (
           <Registry
-            cat={cat}
+            items={items}
           />
         ))}
 
--- client/clunion/src/pages/CategoryPage.js
+++ client/clunion/src/pages/CategoryPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import Category from '../components/Category'
 import App from '../style/App.css'
 
@@ -13,18 +13,26 @@ const CategoryPage = () => {
   let { id } = useParams()
 
   useEffect(() => {
+    let isCancelled = false
     const getCategory = async () => {
       const response = await axios.get(`${BASE_URL}/category/${id}`)
       console.log(response, 'CATEGORY THAT WORKS')
-      setTitle(response.data)
+      if (!isCancelled) {
+        setTitle(response.data)
+      }
     }
     getCategory()
-  }, [])
+    return () => {
+      isCancelled = true
+    }
+  }, [id])
 
   return (
     <div className="categoryPage">
       {title.map((cat) => (
-        <Category id={cat.id} title={cat.title} BASE_URL={BASE_URL} />
+        <Link to={`/category/${cat.id}`} key={cat.id}>
+          <Category {...cat} title={cat.title} BASE_URL={BASE_URL} />
+        </Link>
       ))}
     </div>
   )
