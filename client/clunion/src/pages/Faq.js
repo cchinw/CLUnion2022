186afa97d12eebca882d2bb937f8c3ccf5bab147
Chinwendu <chinwenduukoha@gmail.com>
2022-03-30 21:41:13 -0400

    faq page done

--- client/clunion/src/pages/Faq.js
+++ client/clunion/src/pages/Faq.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const Faq = () => {
-  return <div className="faq"></div>
+  return (
+    <div className="faq">
+      <p>
+        This is a model of my wedding website I built during my software
+        engineering immersive program at General assembly. Check me out on
+        Linkedin!
+      </p>
+      <a href="https://www.linkedin.com/in/chinwenduukoha/">Chinwendu Ukoha</a>
+    </div>
+  )
 }
 
 export default Faq
