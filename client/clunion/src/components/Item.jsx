ed361280d91271c0894b7c6e1b92bf47a71c7473
Chinwendu <chinwenduukoha@gmail.com>
2022-03-30 21:58:57 -0400

    item component done

---
+++ client/clunion/src/components/Item.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Item = ({ item, onClickAdd, onClickMinus }) => {
+
+  
+    
+  return (
+    <div className='Item'>
+      <div className="itemName" >
+        <img src={item.image} alt='food-image-picture'/>
+        <h3>{item.title}</h3>
+        <h5>{item.description}</h5>
+        <h5>{item.price}</h5>
+        <h6>{item.quantityNeeded}</h6>
+        <button onClick={(e) => onClickAdd(e, item._id)}>Add to Cart</button>
+        <button onClick={(e) => onClickMinus(e, item._id)}>Remove from Cart</button>
+      </div>
+    </div>
+  )
+}
+
+export default Item
